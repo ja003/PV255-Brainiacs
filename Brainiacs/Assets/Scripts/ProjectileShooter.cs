9563208d41ec06ecec2d92c0f3d7417ef88b0444
ja003 <ja004@seznam.cz>
2015-10-11 17:12:19 +0200

    prototype_02
    new scene "prototype_02" added
    "prototyp" scene renamed to "prototype_01"

---
+++ Brainiacs/Assets/Scripts/ProjectileShooter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileShooter : MonoBehaviour {
+
+    GameObject prefab;
+    Vector3 forward = new Vector3(1, 0, 0);
+
+    public float speed = 500;
+    public float delayTime = 0.5f;
+    public float counter = 0;
+
+	// Use this for initialization
+	void Start () {
+        prefab = Resources.Load("projectile") as GameObject;
+        delayTime = 0.5f;
+        speed = 500;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKey(KeyCode.Q) && counter > delayTime)
+        {
+            GameObject projectile = Instantiate(prefab) as GameObject;
+            Destroy(projectile.gameObject, 1);
+
+            projectile.transform.position = transform.position + forward;
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+            rb.velocity = forward * speed;
+            counter = 0;
+        }
+
+        counter += 1 * Time.deltaTime;
+	}
+}
---
+++ Brainiacs/Assets/Scripts/ShootingScript.js
@@ -0,0 +1,13 @@
+﻿var projectile : Rigidbody;
+var speed = 10;
+
+function Update () {
+
+    if ( Input.GetButton ("Fire1")) {
+
+        clone = Instantiate(projectile, transform.position, transform.rotation);
+        clone.velocity = transform.TransformDirection( Vector3 (speed, 0, 0));
+
+        Destroy (clone.gameObject, 3);
+
+    }}
