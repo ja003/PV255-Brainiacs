08d784826188234b9d74f5881af9e306d3dbb824
Healadin145 <matus.guoth@gmail.com>
2015-12-01 18:42:07 +0100

    Hratky s MainMenu
    zaklad animovania

--- Brainiacs/Assets/MainMenu/Scripts/MainMenu.cs
+++ Brainiacs/Assets/MainMenu/Scripts/MainMenu.cs
@@ -10,6 +10,8 @@ public class MainMenu : MonoBehaviour {
     public GUIStyle newGameTexture;
     public GUIStyle controlsTexture;
     public GUIStyle quitTexture;
+    public GUIStyle backTexture;
+    public GUIStyle startTexture;
 
     public float headerYCoord;
     public float headerHeight;
@@ -18,31 +20,101 @@ public class MainMenu : MonoBehaviour {
     public float newGamePlacementY;
     public float newGameHeight;
     public float distanceBetweenButtons;
-    
+    public float backButtonPlacementY;
+
+    private float cameraPositionY = 0.0f;
+
+    private bool moveToControls = false;
+    private bool moveToNewGame = false;
+    private bool moveDownToMenu = false;
+    private bool moveUpToMenu = false;
 
     void OnGUI()
     {
         //displays background texture
-        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+        GUI.DrawTexture(new Rect(0, (cameraPositionY - 1) * Screen.height, Screen.width, 3 * Screen.height), backgroundTexture);
+
+        float buttonX = Screen.width * newGamePlacementX;
+        float buttonWidth = (Screen.width) - (Screen.width * newGamePlacementX * 2);
+        float buttonHeight = Screen.height * newGameHeight;
+
+        //-------------------------------NEW GAME SCREEN-------------------------------
+        if (GUI.Button(new Rect(buttonX - (Screen.width / 4), Screen.height * (-1 + cameraPositionY + backButtonPlacementY), buttonWidth, buttonHeight), "", startTexture))
+        {
+            //startgame
+        }
+        if (GUI.Button(new Rect(buttonX + (Screen.width / 4), Screen.height * (-1 + cameraPositionY + backButtonPlacementY), buttonWidth, buttonHeight), "", backTexture))
+        {
+            moveDownToMenu = true;
+        }
+
+        //-------------------------------MENU SCREEN-----------------------------------
 
         //displays header texture
-        GUI.DrawTexture(new Rect(0, Screen.height * headerYCoord, Screen.width, Screen.height * headerHeight), headerTexture);
+        GUI.DrawTexture(new Rect(0, Screen.height * (headerYCoord + cameraPositionY), Screen.width, Screen.height * headerHeight), headerTexture);
 
-        //display buttons
-        float buttonX = Screen.width * newGamePlacementX;
-        float buttonWidth = (Screen.width ) - (Screen.width * newGamePlacementX * 2);
-        float buttonHeight = Screen.height * newGameHeight;
-        if (GUI.Button(new Rect(buttonX, Screen.height * newGamePlacementY, buttonWidth, buttonHeight), "", newGameTexture))
+        //newGamePlacementY += cameraPositionY;
+
+        if (GUI.Button(new Rect(buttonX, Screen.height * (newGamePlacementY + cameraPositionY), buttonWidth, buttonHeight), "", newGameTexture))
         {
-            //game manager
+            moveToNewGame = true;
         }
-        if (GUI.Button(new Rect(buttonX, Screen.height * (newGamePlacementY + distanceBetweenButtons), buttonWidth, buttonHeight), "", controlsTexture))
+        if (GUI.Button(new Rect(buttonX, Screen.height * (newGamePlacementY + distanceBetweenButtons + cameraPositionY), buttonWidth, buttonHeight), "", controlsTexture))
         {
-            //controls
+            moveToControls = true;
         }
-        if (GUI.Button(new Rect(buttonX, Screen.height * (newGamePlacementY + 2 * distanceBetweenButtons), buttonWidth, buttonHeight), "", quitTexture))
+        if (GUI.Button(new Rect(buttonX, Screen.height * (newGamePlacementY + 2 * distanceBetweenButtons + cameraPositionY), buttonWidth, buttonHeight), "", quitTexture))
         {
             Application.Quit();
+        }
+
+        //-------------------------------CONTROLS SCREEN-------------------------------
+        if (GUI.Button(new Rect(buttonX, Screen.height * (1 + cameraPositionY + backButtonPlacementY), buttonWidth, buttonHeight), "", backTexture))
+        {
+            moveUpToMenu = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (moveToControls)
+        {
+            cameraPositionY -= 0.01f;
+            if (cameraPositionY < -0.99f)
+            {
+                cameraPositionY = -1.0f;
+                moveToControls = false;
+            }
+        }
+
+        if (moveToNewGame)
+        {
+            cameraPositionY += 0.01f;
+            if (cameraPositionY > 0.99f)
+            {
+                cameraPositionY = 1.0f;
+                moveToNewGame = false;
+            }
+        }
+
+        if (moveDownToMenu)
+        {
+            cameraPositionY -= 0.01f;
+            if (cameraPositionY < -0.0f)
+            {
+                cameraPositionY = -0.0f;
+                moveDownToMenu = false;
+            }
+        }
+
+        if (moveUpToMenu)
+        {
+            cameraPositionY += 0.01f;
+            if (cameraPositionY > 0.0f)
+            {
+                cameraPositionY = 0.0f;
+                moveUpToMenu = false;
+            }
         }
     }
 }
