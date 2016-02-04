3b39a9f77e91ce1473a0b6571261ec26dea02c2b
Healadin145 <matus.guoth@gmail.com>
2016-02-04 20:27:44 +0100

    Quit ingame button

---
+++ Brainiacs/Assets/Scripts/HUD/QuitInGameButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitInGameButton : MonoBehaviour {
+
+    public GUIStyle backTexture;
+
+    void OnGUI()
+    {
+        float buttonX = (1920.0f - backTexture.normal.background.width) * Screen.width / 1920.0f;
+        float buttonWidth = backTexture.normal.background.width * Screen.width / 1920.0f;
+        float buttonHeight = backTexture.normal.background.height * Screen.height / 1080.0f;
+
+        if (GUI.Button(new Rect(Screen.width * 0.95f, 0.0f, buttonWidth, buttonHeight), "", backTexture))
+        {
+            Application.LoadLevel("MainMenu");
+        }
+    }
+}
