dfc543a601adfc187d87c7696ed3f2cb97d61dad
Healadin145 <matus.guoth@gmail.com>
2016-02-04 20:34:36 +0100

    Quit position fix

--- Brainiacs/Assets/Scripts/HUD/QuitInGameButton.cs
+++ Brainiacs/Assets/Scripts/HUD/QuitInGameButton.cs
@@ -11,7 +11,7 @@ public class QuitInGameButton : MonoBehaviour {
         float buttonWidth = backTexture.normal.background.width * Screen.width / 1920.0f;
         float buttonHeight = backTexture.normal.background.height * Screen.height / 1080.0f;
 
-        if (GUI.Button(new Rect(Screen.width * 0.95f, 0.0f, buttonWidth, buttonHeight), "", backTexture))
+        if (GUI.Button(new Rect(buttonX, 0.0f, buttonWidth, buttonHeight), "", backTexture))
         {
             Application.LoadLevel("MainMenu");
         }
